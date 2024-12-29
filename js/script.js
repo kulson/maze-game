e41ad0e2cf75602d25452aec831a6a0a10f7c5d6
kulson <lukasz.bruczuk@protonmail.com>
2024-12-29 19:16:06 +0100

    29 Dec 2024 19:16:06

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ var pacman;
 var wall;
 var finish;
 
+console.log("haah");
 var game = new Phaser.Game(480, 320, Phaser.AUTO, null, {
   preload: preload,
   create: create,
@@ -23,8 +24,8 @@ function preload() {
   game.scale.pageAlignHorizontally = true;
   game.scale.pageAlignVertically = true;
   game.stage.backgroundColor = "#fff";
-  game.load.image("pacman", "../../assets/pacman-right.png");
-  game.load.image("wall", "../assets/wall.png");
+  game.load.image("pacman", "assets/pacman-right.png");
+  game.load.image("wall", "assets/wall.png");
 }
 
 function create() {
@@ -34,13 +35,60 @@ function create() {
   wall = game.add.sprite(32, 32, "wall");
 }
 
+var directions = [
+  { x: -1, y: 0 },
+  { x: 1, y: 0 },
+  { x: 0, y: -1 },
+  { x: 0, y: 1 },
+];
+
+var pacmanPosition = {
+  x: 0,
+  y: 0,
+};
+
 function update() {}
 
 function onKeyUp(event) {
   switch (event.keyCode) {
     case Keyboard.A:
+      console.log("lewo");
+      moveTo(pacmanPosition, directions[0]);
+      break;
     case Keyboard.D:
+      console.log("prawo");
+      moveTo(pacmanPosition, directions[1]);
+      break;
     case Keyboard.W:
+      console.log("gora");
+      moveTo(pacmanPosition, directions[2]);
+      break;
     case Keyboard.S:
+      console.log("dol");
+      moveTo(pacmanPosition, directions[3]);
+      break;
+  }
+}
+
+function moveTo(pacmanPosition, direction) {
+  var currentPacmanPosition = {
+    x: 0,
+    y: 0,
+  };
+  currentPacmanPosition.x = pacmanPosition.x + direction.x;
+  currentPacmanPosition.y = pacmanPosition.y + direction.y;
+  if (
+    currentPacmanPosition.x >= 0 &&
+    currentPacmanPosition.x <= COLS - 1 &&
+    currentPacmanPosition.y >= 0 &&
+    currentPacmanPosition.y <= ROWS - 1 &&
+    map[currentPacmanPosition.x][currentPacmanPosition.y] === 1
+  )
+    console.log(currentPacmanPosition);
+  if (
+    currentPacmanPosition.x === ROWS - 1 &&
+    currentPacmanPosition.y === COLS - 1
+  ) {
+    console.log("koniec gry");
   }
 }
