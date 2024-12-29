aa233b19c3b54bb71c67e7fc88a3d92efd1d735b
kulson <lukasz.bruczuk@protonmail.com>
2024-12-29 18:56:53 +0100

    29 Dec 2024 18:56:53

--- js/script.js
+++ js/script.js
@@ -9,26 +9,29 @@ var COLS = 15;
 var map;
 
 var pacman;
+var wall;
+var finish;
 
-var game = new Phaser.Game(COLS * FONT * 0.6, ROWS * FONT, Phaser.AUTO, null, {
+var game = new Phaser.Game(480, 320, Phaser.AUTO, null, {
   preload: preload,
   create: create,
   update: update,
 });
 
 function preload() {
-  handleRemoteImagesOnJSFiddle();
   game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
   game.scale.pageAlignHorizontally = true;
   game.scale.pageAlignVertically = true;
-  game.stage.backgroundColor = "#eee";
-  game.load.image("pacman", "assets/pacman-right.png");
+  game.stage.backgroundColor = "#fff";
+  game.load.image("pacman", "../../assets/pacman-right.png");
+  game.load.image("wall", "../assets/wall.png");
 }
 
 function create() {
   // init keyboard commands
   game.input.keyboard.addCallbacks(null, null, onKeyUp);
-  pacman = game.add.sprite(100, 100, "pacman");
+  pacman = game.add.sprite(32, 32, "pacman");
+  wall = game.add.sprite(32, 32, "wall");
 }
 
 function update() {}
