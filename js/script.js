c0af77c6a197d4e28d1567f2e1bf2e2b8e8c27ee
kulson <lukasz.bruczuk@protonmail.com>
2024-12-29 18:13:02 +0100

    29 Dec 2024 18:13:02

--- js/script.js
+++ js/script.js
@@ -9,13 +9,24 @@ var COLS = 15;
 var map;
 
 var game = new Phaser.Game(COLS * FONT * 0.6, ROWS * FONT, Phaser.AUTO, null, {
+  preload: preload,
   create: create,
+  update: update,
 });
+
+function preload() {
+  handleRemoteImagesOnJSFiddle();
+  game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+  game.scale.pageAlignHorizontally = true;
+  game.scale.pageAlignVertically = true;
+}
 
 function create() {
   // init keyboard commands
   game.input.keyboard.addCallbacks(null, null, onKeyUp);
 }
+
+function update() {}
 
 function onKeyUp(event) {
   switch (event.keyCode) {
