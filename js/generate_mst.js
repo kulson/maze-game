1b8212bab5253ff9431bdefdd10c77b71ebe35f3
rpecherz <rafal123pech@gmail.com>
2024-12-29 21:48:53 +0100

    change for it to already be a maze lol

--- js/generate_mst.js
+++ js/generate_mst.js
@@ -1,7 +1,7 @@
 function generate(n) {
     let kol = [];
-    let anc = new Array(n);
-    let mass = new Array(n).fill(1);
+    let anc = new Array(n*n);
+    let mass = new Array(n*n).fill(1);
 
     function find_ancestor(node, anc) {
         if(anc[node] !== node)
@@ -12,11 +12,10 @@ function generate(n) {
     function kruskal() {
         kol.sort((a, b) => a[0] - b[0]);
 
-        let mst = Array.from({ length: n }, () => Array(n).fill(0));
+        let mst = Array.from({ length: 2*n-1 }, () => Array(2*n-1).fill(1));
 
         while (kol.length) {
-            let [_, u, v] = kol.pop();
-            if (mst[u][v] === 1) continue;
+            let [_, u, v, k] = kol.pop();
 
             let first_anc = find_ancestor(u, anc);
             let second_anc = find_ancestor(v, anc);
@@ -29,24 +28,47 @@ function generate(n) {
                     anc[second_anc] = first_anc;
                     mass[first_anc] += mass[second_anc];
                 }
+                
 
-                mst[u][v] = 1;
-                mst[v][u] = 1; 
+                let u1 = Math.floor(u / n);
+                let v1 = Math.floor(v / n);
+
+                if(k == 0)
+                {
+                    mst[2*u1-1][(u%n)*2] = 0;
+                }
+                else
+                {
+                    mst[2*u1][(u%n)*2+1] = 0;
+                }
             }
+            for(let i=0;i<2*n;i+=2)
+                for(let j=0;j<2*n;j+=2)
+                    mst[i][j] = 0;
         }
 
         return mst;
     }
 
     for(let i = 0; i < n; ++i) {
-        anc[i] = i;
 
         for (let j = 0; j < n; ++j) {
-            if (i === j) continue;
-            let weight = Math.floor(Math.random() * (n - Math.sqrt(n))) + n;
-            kol.push([weight, i, j]);
+            anc[i*n+j] = i*n+j;
+            if(i>0)
+            {
+                //kierunek 
+                // 0 - gora
+                // 1 - prawo
+                let weight = Math.floor(Math.random() * (n - Math.sqrt(n))) + n;
+                kol.push([weight, i*n+j,(i-1)*n+j,0]);
+            }
+            if(j < n-1)
+            {
+                let weight = Math.floor(Math.random() * (n - Math.sqrt(n))) + n;
+                kol.push([weight, i*n+j, i*n+j+1,1]);
+            }
         }
     }
     return kruskal();
 }
-
+console.log(generate(3))
